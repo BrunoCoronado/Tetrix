6fb3f892f4760b287c70d34d379f863049bad5f4
Bruno Coronado <bcoronado.morales@gmail.com>
2019-08-05 17:23:11 -0600

    Proyecto

---
+++ cmake-build-debug/CMakeFiles/Tetrix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetrix.dir/main.c.o"
+  "Tetrix.pdb"
+  "Tetrix"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tetrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
